ee8bc057fbda90e72df44543bd37446de805c08a
dongtony12 <dongtony0523@naver.com>
2023-05-30 08:24:44 +0900

    avd 세팅 및 script 파일 생성

---
+++ startAndroid.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+
+const androidProjectRoot = '/Users/kimdonghyeun/develop/walk-fight-android'
+
+const buildCommand = `${androidProjectRoot}/gradlew assembleDebug`
+
+const emulatorCommand = `~/Library/Android/sdk/emulator/emulator -avd ${process.env.AVD_NAME}`
+
+exec(buildCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error building project: ${error.message}`)
+    return
+  }
+
+  console.log(stdout)
+
+  // If the build succeeded, start the emulator.
+  exec(emulatorCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error starting emulator: ${error.message}`)
+      return
+    }
+
+    console.log(stdout)
+  })
+})
