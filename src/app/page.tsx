41c46c7a3107ee25766a9a4a4eb591eeed7a4aae
Hanbyoul <dytjqdla120@naver.com>
2023-06-04 21:23:19 +0900

    tailwind 테마 컬러 설정,screen size 재설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <div>home</div>
-    </div>
-  )
+  return <div>home</div>
 }
-
-/**
- *  TODO : src폴더 생성 및 component,hooks,util 폴더 생성 하기
- */
