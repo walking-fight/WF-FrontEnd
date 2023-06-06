7dd38a7ba54c71d8f559ab42881feb43152e5356
dongtony12 <dongtony0523@naver.com>
2023-06-06 10:42:21 +0900

    startAndroid file build command 삭제

--- startAndroid.js
+++ startAndroid.js
@@ -4,23 +4,24 @@ const androidProjectRoot = '/Users/kimdonghyeun/develop/walk-fight-android'
 
 const buildCommand = `${androidProjectRoot}/gradlew assembleDebug`
 
-const emulatorCommand = `~/Library/Android/sdk/emulator/emulator -avd ${process.env.AVD_NAME}`
+// const emulatorCommand = `~/Library/Android/sdk/emulator/emulator -avd ${process.env.AVD_NAME}`
+const emulatorCommand = `~/Library/Android/sdk/emulator/emulator -avd Resizable_Experimental_API_33`
 
-exec(buildCommand, (error, stdout, stderr) => {
+// exec(buildCommand, (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`Error building project: ${error.message}`)
+//     return
+//   }
+
+//   console.log(stdout)
+
+// If the build succeeded, start the emulator.
+exec(emulatorCommand, (error, stdout, stderr) => {
   if (error) {
-    console.error(`Error building project: ${error.message}`)
+    console.error(`Error starting emulator: ${error.message}`)
     return
   }
 
   console.log(stdout)
-
-  // If the build succeeded, start the emulator.
-  exec(emulatorCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error starting emulator: ${error.message}`)
-      return
-    }
-
-    console.log(stdout)
-  })
 })
+// })
